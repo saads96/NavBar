879d7e6aea7af039a7f57cd8be567fd121c07836
saads96 <37954514+saads96@users.noreply.github.com>
2024-05-19 14:24:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const navbarEl = document.querySelector(".navbar");
+
+const bottomContainerEl = document.querySelector(".bottom-container");
+
+console.log("navbarEl.offsetTopHeight");
+console.log("bottomContainerEl.offsetTop");
+
+window.addEventListener("scroll", () => {
+console.log(window.scrollY);
+});
